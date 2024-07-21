9bbf1f6fabae36d5641e261fae3996345ea3f2b0
Aviral Srivastava <kanha23srivastava@gmail.com>
2024-07-21 22:40:19 +0530

    added colors to the variables and maked profile pic visualized on the home screen in app bar

---
+++ lib/color/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const color1 = Color.fromARGB(255, 69, 53, 1963);
+const color2 = Color.fromARGB(255, 71, 140, 207);
+const color3 = Color.fromARGB(255, 54, 194, 206);
+const color4 = Color.fromARGB(255, 119, 228, 200);
+const color5 = Color.fromARGB(255, 34, 35, 35);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_app/color/colors.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
+   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ToDO App'),) ,
+      appBar: AppBar(
+        backgroundColor: color4,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: color5,
+              size: 30,
+            ),
+            Container(
+              height: 55,
+              width: 55,
+              child: ClipRRect(
+                borderRadius : BorderRadius.circular(20),
+                child: Image.asset('assets/images/profile-pic.png',),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Container(
         child: Text('This is the Home Screen'),
       ),
